8b07ef47b85a85008d90f3559ceb7012dbbc3eb3
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-27 13:50:02 -0700

    Modified zoomlocaton; Added search capabilities for sushi and lobster rolls

--- LocalRestaurants/ViewController.m
+++ LocalRestaurants/ViewController.m
@@ -35,21 +35,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     CLLocationCoordinate2D zoomLocation;
-    zoomLocation.latitude = 33.109084;
-    zoomLocation.longitude = -117.288364;
+    zoomLocation.latitude = 42.364506;
+    zoomLocation.longitude = -71.057899;
     
-    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 10000, 10000);
+    
+    
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 1000, 1000);
     [_mapView setRegion:viewRegion animated:YES];
     _mapView.showsUserLocation = YES;
     
     MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc] init];
-    request.naturalLanguageQuery = @"Restaurants";
+    request.naturalLanguageQuery = @"Lobster Rolls";
     request.region = _mapView.region;
     MKLocalSearch *search = [[MKLocalSearch alloc] initWithRequest:request];
-    [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+    /*[search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
         NSLog(@"Map Items: %@", response.mapItems);
         
-    }];
+    }];*/
     [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
         for (MKMapItem *item in response.mapItems) {
             MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
@@ -57,8 +59,18 @@
             annotation.coordinate = item.placemark.coordinate;
             [_mapView addAnnotation:annotation];
         }}];
-    
-
+    MKLocalSearchRequest *request2 = [[MKLocalSearchRequest alloc] init];
+    request2.naturalLanguageQuery = @"Sushi";
+    request2.region = _mapView.region;
+    MKLocalSearch *search2 = [[MKLocalSearch alloc] initWithRequest:request2];
+    [search2 startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+        for (MKMapItem *item in response.mapItems) {
+            MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+            annotation.title = item.name;
+            annotation.coordinate = item.placemark.coordinate;
+            [_mapView addAnnotation:annotation];
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
